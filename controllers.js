7744c9f58a23996a91520137ad2be04155b072e3
saroyar <yarsaro2001@gmail.com>
2023-03-15 16:02:25 +0530

    version 1 of my project

--- controllers.js
+++ controllers.js
@@ -1,73 +1,140 @@
-const pool=require("./pgConnect")
-const jwt=require('jsonwebtoken');
+const pool = require("./pgConnect");
+const jwt = require("jsonwebtoken");
 
+
+// controller for get all user
 
 const getUsers = (request, response) => {
-    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+  try {
+    pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
       if (error) {
-        throw error
+        return response.status(400).json({
+          success: false,
+          message: error.message,
+        });
       }
-      response.status(200).json(results.rows)
-    })
+      response.status(200).json(results.rows);
+    });
+  } catch (error) {
+    response.status(400).json({
+      success: false,
+      message: error.message,
+    });
   }
-  
-  const getUserById = (request, response) => {
-    const id = parseInt(request.params.id)
-  
-    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-  }
-  
-  const createUser = (request, response) => {
-    const { name, email } = request.body
-  
-    pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
-      if (error) {
-        throw error
-      }
+};
 
-      const token=jwt.sign({email:email},process.env.key);
-  
-      console.log(token);
-      response.cookie("token",token).status(201).send("user created");
-    })
+
+// controller for get rhe user by id
+
+const getUserById = (request, response) => {
+  try {
+    const id = parseInt(request.params.id);
+
+    pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+      if (error) {
+        return response.status(400).json({
+          success: false,
+          message: error.message,
+        });
+      }
+      response.status(200).json(results.rows);
+    });
+  } catch (error) {
+    response.status(400).json({
+      success: false,
+      message: error.message,
+    });
   }
-  
-  const updateUser = (request, response) => {
-    const id = parseInt(request.params.id)
-    const { name, email } = request.body
-  
+};
+
+
+// controller for create new user
+const createUser = (request, response) => {
+  try {
+    const { name, email } = request.body;
+
     pool.query(
-      'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+      "INSERT INTO users (name, email) VALUES ($1, $2)",
+      [name, email],
+      (error, results) => {
+        if (error) {
+          return response.status(400).json({
+            success: false,
+            message: error.message,
+          });
+        }
+
+        const token = jwt.sign({ email: email }, process.env.key);
+
+        console.log(token);
+        response.cookie("token", token).status(201).send("user created");
+      }
+    );
+  } catch (error) {
+    response.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+// controller for update user
+
+const updateUser = (request, response) => {
+  try {
+    const id = parseInt(request.params.id);
+    const { name, email } = request.body;
+
+    pool.query(
+      "UPDATE users SET name = $1, email = $2 WHERE id = $3",
       [name, email, id],
       (error, results) => {
         if (error) {
-          throw error
+          return response.status(400).json({
+            success: false,
+            message: error.message,
+          });
         }
-        response.status(200).send(`User modified with ID: ${id}`)
+        response.status(200).send(`User modified with ID: ${id}`);
       }
-    )
+    );
+  } catch (error) {
+    response.status(400).json({
+      success: false,
+      message: error.message,
+    });
   }
-  
-  const deleteUser = (request, response) => {
-    const id = parseInt(request.params.id)
-  
-    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+};
+
+
+// controller for delete one user
+
+const deleteUser = (request, response) => {
+  try {
+    const id = parseInt(request.params.id);
+
+    pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
       if (error) {
-        throw error
+        return response.status(400).json({
+          success: false,
+          message: error.message,
+        });
       }
-      response.status(200).send(`User deleted with ID: ${id}`)
-    })
+      response.status(200).send(`User deleted with ID: ${id}`);
+    });
+  } catch (error) {
+    response.status(400).json({
+      success: false,
+      message: error.message,
+    });
   }
-  
-  module.exports = {
-    getUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser,
-  }
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
--- routes.js
+++ routes.js
@@ -5,10 +5,10 @@ const { auth } = require("./middleware/Auth");
 const router=Router();
 
 
-router.get('/users',auth, dbMethod.getUsers)
-router.get('/users/:id', dbMethod.getUserById)
+router.get('/users', dbMethod.getUsers)
+router.get('/users/:id',auth, dbMethod.getUserById)
 router.post('/users', dbMethod.createUser)
-router.put('/users/:id', dbMethod.updateUser)
-router.delete('/users/:id', dbMethod.deleteUser)
+router.put('/users/:id',auth, dbMethod.updateUser)
+router.delete('/users/:id',auth, dbMethod.deleteUser)
 
 module.exports=router
